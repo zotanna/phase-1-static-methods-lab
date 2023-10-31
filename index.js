12d455436ccea7347ea2fa2a2a2ed5d481e72137
denis <denniiskim@gmail.com>
2023-10-31 04:43:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input) {
+    if (typeof input !== 'string' || input.length === 0) {
+      return input;
+    }
+
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+
+  static sanitize(input) {
+    if (typeof input !== 'string' || input.length === 0) {
+      return input;
+    }
+
+    return input.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(input) {
+    if (typeof input !== 'string' || input.length === 0) {
+      return input;
+    }
+
+    const excludedWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+
+    const words = input.toLowerCase().split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (i === 0 || !excludedWords.includes(word)) {
+        words[i] = Formatter.capitalize(word);
+      }
+    }
+    return words.join(' ');
+  }
 }
